00ff422434f5933cb40116770b1791c36a8cba73
Sherigabia <orionsavvy@gmail.com>
2023-01-03 15:47:35 +0000

    new Server url updated with new API Keys

--- client/script.js
+++ client/script.js
@@ -78,7 +78,7 @@ const handleSubmit = async (e) => {
 
   loader(messageDiv)
   //fetch data from server
-  const response = await fetch('https://codex-066n.onrender.com/',
+  const response = await fetch('https://codex-066n.onrender.com',
    {
     method: 'POST',
     headers: {
